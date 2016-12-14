966f2a376b24c1826aba8c4773e7380994a39f65
ambassador <ambassadortsure@gmail.com>
2016-12-14 20:20:14 +0300

    ORM. Simplification_1.

--- orm/orm.rb
+++ orm/orm.rb
@@ -9,67 +9,33 @@ class ORM
   attr_reader :connect
 
   def create(tbl, data)
-    i=0
-    fields = ""
-    values = ""
-    data.each do |key, value|
-      i += 1
-      fields += key
-      if value.class == String
-        values += "'" + value + "'"
-      else
-        values += value.to_s
-      end
-      if i < data.length
-        fields += "," 
-        values += ","
-      end
-    end
-    makeDML("INSERT INTO #{tbl} (#{fields}) values (#{values});").cmd_tuples.to_s + " records were created"
+    make_DML("INSERT INTO #{tbl} (#{data.keys.join(",")}) values (#{data.values.collect{|x| x.class==String ? "'"+x+"'" : x}.join(",")});").cmd_tuples.to_s + " records were created"
   end
 
   def update(tbl, data, cond)
-    updateble = make_attr_str(data)
-    conditions = make_attr_str(cond)
-    makeDML("UPDATE #{tbl} SET #{updateble} WHERE #{conditions};").cmd_tuples.to_s + " records were changed"
+    make_DML("UPDATE #{tbl} SET #{make_attr_str(data)} WHERE #{make_attr_str(cond)};").cmd_tuples.to_s + " records were changed"
   end
 
   def delete(tbl, cond)
-    conditions = make_attr_str(cond)
-    makeDML("DELETE FROM #{tbl} WHERE #{conditions};").cmd_tuples.to_s + " records were deleted"
+    make_DML("DELETE FROM #{tbl} WHERE #{make_attr_str(cond)};").cmd_tuples.to_s + " records were deleted"
   end
 
   def find(tbl, cond)
-    conditions = make_attr_str(cond)
-    result = makeDML("SELECT * FROM #{tbl} WHERE #{conditions};")
+    result = make_DML("SELECT * FROM #{tbl} WHERE #{make_attr_str(cond)};")
     if result.ntuples > 0
-      result.each do |row|
-        return row
-      end
+      arr = []
+      result.each {|row| arr.push(row)}
+      return arr
     end
     result.ntuples.to_s + " records were selected"
   end
 
   private
-    def makeDML(executionString)
-      @connect.exec(executionString)
+    def make_DML(execution_string)
+      @connect.exec(execution_string)
     end
 
     def make_attr_str(hash)
-      i = 0
-      result = ""
-      hash.each do |key, value|
-        i += 1
-        result += key + "="
-        if value.class == String
-          result += "'" + value + "'"
-        else
-          result += value.to_s
-        end
-        if i < hash.length
-          result += " and "
-        end
-      end
-      result
+      hash.to_a.collect{|x| [x[0],x[1].class==String ? "'"+x[1]+"'" : x[1]].join("=")}.join(" and ")
     end
 end
--- orm/user.rb
+++
@@ -1,8 +0,0 @@
-class User
-	def initialize(attributes)
-		@login = attributes[:login]
-		@password = attributes[:password]
-	end
-
-	attr_accessor :login, :password
-end
